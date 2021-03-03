420593d0a5487d54c0288926c82646f0d34e8384
Darius Sparks <dariussparks19@gmail.com>
2021-03-02 16:32:34 -0800

    final commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,19 +11,19 @@ const Nav = () =>{
     return(
         <StyledNav>
             <h1>
-                <Link id="logo" to="/portfolio">
+                <Link id="logo" to="/portfolio/">
                     Portfolio 
                 </Link>
             </h1>
             <ul>
                 <li>
-                    <Link to="/portfolio">
+                    <Link to="/portfolio/">
                         About Me
                     </Link>
                     <Line 
                         transition={{duration: 0.75}} 
                         initial={{width: "0%"}}
-                        animate={{width: pathname === '/portfolio' ? '50%' : '0%'}}
+                        animate={{width: pathname === '/portfolio/' ? '50%' : '0%'}}
                     />
                 </li>
                 <li>
